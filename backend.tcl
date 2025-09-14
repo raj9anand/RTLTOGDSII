4d8ad02a07904eed6fb19a3f62b8269c9dde2f11
Anand Raj <60174790+raj9anand@users.noreply.github.com>
2025-09-14 10:55:24 +0530

    Update backend.tcl

--- backend.tcl
+++ backend.tcl
@@ -1,7 +1,26 @@
-##pinplacement_command
+###floorplan_command###
+#done using gui mode
+
+##pinplacement_command#####
 checkPinAssignment                       ###checkpin
 legalizePin -pin * -moveFixedpin         ###legalizepin
 setPtnPinStatus -pin * -status fixed     ###fixpin at location
+verify_drc                               ##drc_check
+saveDesign ./pd_output/pinplacement.enc      #save ur pinplacement design
+
+###powerplanning_command#########
+#it is completly done using gui mode.
+#must check drc after powerplanning
+verify_drc
+saveDesign  ./pd_output/mod10counter_powerplan.enc    #save ur powerplan design
+
+##placement_command
+
+
+
+
+
+
 
 
 
