0aa2a48af68c2348c3e5c0fa355f52674eb6f09e
Anand Raj <60174790+raj9anand@users.noreply.github.com>
2025-11-27 09:33:48 +0530

    Update extra_commands.tcl

--- extra_commands.tcl
+++ extra_commands.tcl
@@ -7,5 +7,6 @@ get_ports
 get_db design .ports
 check_timing_intent   /// print a timing lint report
 write_sdf
-
+report_power
+report_inst_power <g335>     //instead of g335 put std cell instance name
 
